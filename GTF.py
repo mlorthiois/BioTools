f6c88a5b38a08cf22ad820f8fd7be6f5be364cf3
mlorthiois <matthias.lts@gmail.com>
2021-04-10 17:29:50 +0200

    Add parse method with lv1/lv2/lv3 structure

--- GTF.py
+++ GTF.py
@@ -1,3 +1,4 @@
+#! /usr/bin/python3
 class GTFRecord:
     def __init__(self, line):
         line = line.rstrip().split("\t")
@@ -49,35 +50,97 @@ class GTFRecord:
             if x != ""
         )
 
+class GTFRecordWithChildren(GTFRecord):
+    def __init__(self, line):
+        super().__init__(line)
+        self.children = []
+
+    def add_child(self, child):
+        self.children.append(child)
+
+    def format_to_gtf(self):
+        gtf_seq = str(self) + "\n"
+        for child in self.children:
+            if isinstance(child, GTFRecordWithChildren):
+                gtf_seq += child.format_to_gtf() + "\n"
+            elif isinstance(child, GTFRecord):
+                gtf_seq += str(child) + "\n"
+        return gtf_seq.rstrip()
+
+
+
+class Gene(GTFRecordWithChildren):
+    @property
+    def transcripts(self):
+        return [transcript for transcript in self.children]
+
+    @property
+    def exons(self):
+        return [exon for transcript in self.transcripts for exon in transcript.exons]
+
+class Transcript(GTFRecordWithChildren):
+    @property
+    def exons(self):
+        return self.children
 
 class GTF:
     @staticmethod
-    def parse(file, feature=None, strand=None, attributes=None):
+    def parse(file, feature=None, strand=None, attributes=None, by_line=False):
         # Pass attributes as {"gene_id":"ENSG001", "transcript_biotype":"lncRNA"}
         with open(file) as fd:
-            for line in fd:
-                record = GTFRecord(line)
-                if (feature is not None) and (record.feature != feature):
-                    continue
+            if by_line:
+                for line in fd:
+                    if line.startswith("#"):
+                        continue
+                    record = GTFRecord(line)
+                    if (feature is not None) and (record.feature != feature):
+                        continue
 
-                if strand is not None and record.strand != strand:
-                    continue
+                    if strand is not None and record.strand != strand:
+                        continue
 
-                attributes_check = True
-                for attribute, value in attributes.items():
-                    if attribute not in record or record[attribute] != value:
-                        attributes_check = False
-                        break
+                    attributes_check = True
+                    for attribute, value in attributes.items():
+                        if attribute not in record or record[attribute] != value:
+                            attributes_check = False
+                            break
 
-                if attributes_check:
-                    yield record
-                else:
-                    continue
+                    if attributes_check:
+                        yield record
+                    else:
+                        continue
+            else:
+                gene = None
+                for line in fd:
+                    if line.startswith("#"):
+                        continue
+                    
+                    record = GTFRecord(line)
+                    if record.feature == "gene" and gene is not None:
+                        yield gene
+                    if record.feature == "gene":
+                        gene = Gene(line)
+                    elif record.feature == "transcript":
+                        transcript = Transcript(line)
+                        gene.add_child(transcript)
+                    else:
+                        transcript.add_child(record)
+                        
+                yield gene
+
+
+    def reconstruct_full_gtf(file):
+        pass
+
+
 
 
 if __name__ == "__main__":
-    for line in GTF.parse(
-        "short_jeq.gtf",
+    import sys
+    for gene in GTF.parse(
+        sys.argv[1],
         attributes={"gene_id": "XLOC_000003", "transcript_id": "TCONS_00000004"},
     ):
-        print(line)
+        # print(len(gene.transcripts))
+        print(gene.format_to_gtf())
+
